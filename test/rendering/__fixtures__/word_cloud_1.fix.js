5760f29466edfb81e9ae765055ef1be18f0cb919
caele <tsm@qlik.com>
2023-05-24 14:23:53 +0200

    chore: add rendering tests

---
+++ test/rendering/__fixtures__/word_cloud_1.fix.js
@@ -0,0 +1,440 @@
+export default () => {
+  return {
+    genericObjects: [
+      {
+        getLayout() {
+          return {
+            qInfo: {
+              qId: "zbNHWs",
+              qType: "sn-word-cloud",
+            },
+            qSelectionInfo: {
+              qInSelections: false,
+              qMadeSelections: false,
+            },
+            qHyperCube: {
+              qSize: {
+                qcx: 2,
+                qcy: 10,
+              },
+              qDimensionInfo: [
+                {
+                  qFallbackTitle: "Product Group",
+                  qApprMaxGlyphCount: 19,
+                  qCardinal: 17,
+                  qSortIndicator: "A",
+                  qGroupFallbackTitles: ["Product Group"],
+                  qGroupPos: 0,
+                  qStateCounts: {
+                    qLocked: 0,
+                    qSelected: 0,
+                    qOption: 17,
+                    qDeselected: 0,
+                    qAlternative: 0,
+                    qExcluded: 0,
+                    qSelectedExcluded: 0,
+                    qLockedExcluded: 0,
+                  },
+                  qTags: ["$ascii", "$text"],
+                  qDimensionType: "D",
+                  qGrouping: "N",
+                  qNumFormat: {
+                    qType: "R",
+                    qnDec: 14,
+                    qUseThou: 1,
+                    qFmt: "##############",
+                    qDec: ".",
+                    qThou: ",",
+                  },
+                  qIsAutoFormat: true,
+                  qGroupFieldDefs: ["Product Group Desc"],
+                  qMin: "NaN",
+                  qMax: "NaN",
+                  qAttrExprInfo: [],
+                  qAttrDimInfo: [
+                    {
+                      qCardinal: 17,
+                      qSize: {
+                        qcx: 2,
+                        qcy: 8,
+                      },
+                      qFallbackTitle: "Product Group",
+                      id: "colorByAlternative",
+                      colorMapRef: "rAWLLGj",
+                    },
+                  ],
+                  qCardinalities: {
+                    qCardinal: 17,
+                    qHypercubeCardinal: 10,
+                  },
+                  title: "Product Group",
+                  coloring: {
+                    colorMapRef: "rAWLLGj",
+                    changeHash: "0.6784502254237292",
+                  },
+                  autoSort: true,
+                  cId: "fJemSj",
+                  othersLabel: "Others",
+                },
+              ],
+              qMeasureInfo: [
+                {
+                  qFallbackTitle: "# of Products",
+                  qApprMaxGlyphCount: 3,
+                  qCardinal: 0,
+                  qSortIndicator: "D",
+                  qNumFormat: {
+                    qType: "I",
+                    qnDec: 0,
+                    qUseThou: 1,
+                    qFmt: "###0",
+                    qDec: ".",
+                  },
+                  qMin: 27,
+                  qMax: 143,
+                  qIsAutoFormat: true,
+                  qAttrExprInfo: [],
+                  qAttrDimInfo: [],
+                  coloring: {
+                    baseColor: {
+                      color: "#ec00d7",
+                      index: -1,
+                    },
+                    gradient: {
+                      colors: [
+                        {
+                          color: "#ff009f",
+                          index: -1,
+                        },
+                        {
+                          color: "#13ff00",
+                          index: -1,
+                        },
+                        {
+                          color: "#ff0033",
+                          index: -1,
+                        },
+                        {
+                          color: "#1e07ec",
+                          index: -1,
+                        },
+                      ],
+                      breakTypes: [false, true, false],
+                      limits: [0.1, 0.503, 1],
+                      limitType: "percent",
+                    },
+                  },
+                  autoSort: true,
+                  cId: "mKnua",
+                  numFormatFromTemplate: true,
+                },
+              ],
+              qEffectiveInterColumnSortOrder: [1, 0],
+              qGrandTotalRow: [
+                {
+                  qText: "824",
+                  qNum: 824,
+                  qElemNumber: -1,
+                  qState: "X",
+                  qIsTotalCell: true,
+                },
+              ],
+              qDataPages: [
+                {
+                  qMatrix: [
+                    [
+                      {
+                        qText: "Produce",
+                        qNum: "NaN",
+                        qElemNumber: 12,
+                        qState: "O",
+                        qAttrDims: {
+                          qValues: [
+                            {
+                              qText: "Produce",
+                              qElemNo: 12,
+                            },
+                          ],
+                        },
+                      },
+                      {
+                        qText: "130",
+                        qNum: 130,
+                        qElemNumber: 0,
+                        qState: "L",
+                      },
+                    ],
+                    [
+                      {
+                        qText: "Snack Foods",
+                        qNum: "NaN",
+                        qElemNumber: 14,
+                        qState: "O",
+                        qAttrDims: {
+                          qValues: [
+                            {
+                              qText: "Snack Foods",
+                              qElemNo: 14,
+                            },
+                          ],
+                        },
+                      },
+                      {
+                        qText: "107",
+                        qNum: 107,
+                        qElemNumber: 0,
+                        qState: "L",
+                      },
+                    ],
+                    [
+                      {
+                        qText: "Frozen Foods",
+                        qNum: "NaN",
+                        qElemNumber: 10,
+                        qState: "O",
+                        qAttrDims: {
+                          qValues: [
+                            {
+                              qText: "Frozen Foods",
+                              qElemNo: 10,
+                            },
+                          ],
+                        },
+                      },
+                      {
+                        qText: "93",
+                        qNum: 93,
+                        qElemNumber: 0,
+                        qState: "L",
+                      },
+                    ],
+                    [
+                      {
+                        qText: "Canned Foods",
+                        qNum: "NaN",
+                        qElemNumber: 5,
+                        qState: "O",
+                        qAttrDims: {
+                          qValues: [
+                            {
+                              qText: "Canned Foods",
+                              qElemNo: 5,
+                            },
+                          ],
+                        },
+                      },
+                      {
+                        qText: "88",
+                        qNum: 88,
+                        qElemNumber: 0,
+                        qState: "L",
+                      },
+                    ],
+                    [
+                      {
+                        qText: "Dairy",
+                        qNum: "NaN",
+                        qElemNumber: 7,
+                        qState: "O",
+                        qAttrDims: {
+                          qValues: [
+                            {
+                              qText: "Dairy",
+                              qElemNo: 7,
+                            },
+                          ],
+                        },
+                      },
+                      {
+                        qText: "80",
+                        qNum: 80,
+                        qElemNumber: 0,
+                        qState: "L",
+                      },
+                    ],
+                    [
+                      {
+                        qText: "Baking Goods",
+                        qNum: "NaN",
+                        qElemNumber: 2,
+                        qState: "O",
+                        qAttrDims: {
+                          qValues: [
+                            {
+                              qText: "Baking Goods",
+                              qElemNo: 2,
+                            },
+                          ],
+                        },
+                      },
+                      {
+                        qText: "72",
+                        qNum: 72,
+                        qElemNumber: 0,
+                        qState: "L",
+                      },
+                    ],
+                    [
+                      {
+                        qText: "Beverages",
+                        qNum: "NaN",
+                        qElemNumber: 3,
+                        qState: "O",
+                        qAttrDims: {
+                          qValues: [
+                            {
+                              qText: "Beverages",
+                              qElemNo: 3,
+                            },
+                          ],
+                        },
+                      },
+                      {
+                        qText: "59",
+                        qNum: 59,
+                        qElemNumber: 0,
+                        qState: "L",
+                      },
+                    ],
+                    [
+                      {
+                        qText: "Deli",
+                        qNum: "NaN",
+                        qElemNumber: 8,
+                        qState: "O",
+                        qAttrDims: {
+                          qValues: [
+                            {
+                              qText: "Deli",
+                              qElemNo: 8,
+                            },
+                          ],
+                        },
+                      },
+                      {
+                        qText: "53",
+                        qNum: 53,
+                        qElemNumber: 0,
+                        qState: "L",
+                      },
+                    ],
+                    [
+                      {
+                        qText: "-",
+                        qNum: "NaN",
+                        qElemNumber: -2,
+                        qState: "L",
+                        qAttrDims: {
+                          qValues: [
+                            {
+                              qElemNo: -2,
+                            },
+                          ],
+                        },
+                        qIsNull: true,
+                      },
+                      {
+                        qText: "27",
+                        qNum: 27,
+                        qElemNumber: 0,
+                        qState: "L",
+                      },
+                    ],
+                    [
+                      {
+                        qText: "Others",
+                        qNum: "NaN",
+                        qElemNumber: -3,
+                        qState: "L",
+                        qIsOtherCell: true,
+                        qAttrDims: {
+                          qValues: [
+                            {
+                              qElemNo: -2,
+                            },
+                          ],
+                        },
+                      },
+                      {
+                        qText: "143",
+                        qNum: 143,
+                        qElemNumber: 0,
+                        qState: "L",
+                      },
+                    ],
+                  ],
+                  qTails: [
+                    {
+                      qUp: 0,
+                      qDown: 0,
+                    },
+                  ],
+                  qArea: {
+                    qLeft: 0,
+                    qTop: 0,
+                    qWidth: 2,
+                    qHeight: 10,
+                  },
+                },
+              ],
+              qPivotDataPages: [],
+              qStackedDataPages: [],
+              qMode: "S",
+              qNoOfLeftDims: -1,
+              qTreeNodesOnDim: [],
+              qColumnOrder: [],
+              qHasOtherValues: true,
+            },
+            showTitles: true,
+            title: "",
+            subtitle: "",
+            footnote: "",
+            showDetails: false,
+            donut: {
+              showAsDonut: false,
+            },
+            dimensionTitle: true,
+            dataPoint: {
+              auto: true,
+              labelMode: "share",
+            },
+            color: {
+              auto: false,
+              mode: "byDimension",
+              useBaseColors: "off",
+              paletteColor: {
+                index: 12,
+              },
+              useDimColVal: true,
+              useMeasureGradient: true,
+              persistent: false,
+              expressionIsColor: true,
+              expressionLabel: "",
+              measureScheme: "sg",
+              reverseScheme: false,
+              dimensionScheme: "12",
+              autoMinMax: true,
+              measureMin: 0,
+              measureMax: 10,
+              byDimDef: {
+                label: "rAWLLGj",
+                key: "rAWLLGj",
+                type: "libraryItem",
+                activeDimensionIndex: 0,
+              },
+              altLabel: "rAWLLGj",
+            },
+            legend: {
+              show: true,
+              dock: "top",
+              showTitle: true,
+            },
+            visualization: "sn-word-cloud",
+            qHasSoftPatches: false,
+          };
+        },
+        getEffectiveProperties: {},
+      },
+    ],
+  };
+};
